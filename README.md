6eda6fefab635a197a8be59b3d12979b17859708
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-04-05 16:58:23 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dotfiles
+
+A repo with my dotfiles and `setup.sh` script that uses stow to setup the files.
---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+git submodule init
+git submodule update
+
+base=(
+    zsh
+)
+
+useronly=(
+#     git
+)
+
+stowit() {
+    usr=$1
+    app=$2
+    # -v verbose
+    # -R recursive
+    # -t target
+    stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: ${whoami}"
+
+# install apps available to local users and root
+for app in ${base[@]}; do
+    stowit "${HOME}" $app 
+done
+
+# install only user space folders
+for app in ${useronly[@]}; do
+    if [[ ! "$(whoami)" = *"root"* ]]; then
+        stowit "${HOME}" $app 
+    fi
+done
+
+echo ""
+echo "##### ALL DONE"
