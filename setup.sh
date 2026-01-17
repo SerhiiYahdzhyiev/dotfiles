0bc3664de36f710c87ce470a502cc5db972f864a
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2026-01-17 19:12:57 +0100

    Extend setup list of default packages.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 DFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-apps=(git vim zsh wezterm zellij)
+apps=(git vim zsh wezterm zellij node)
 
 if [ "$#" -gt 0 ]; then
         apps=("$@")
