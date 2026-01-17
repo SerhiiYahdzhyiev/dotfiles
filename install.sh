e3b5d4e2bc9043e78383852bd1201aeba803436a
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2026-01-17 17:24:20 +0100

    Update/extend install script.

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install zellij
 cargo install ripgrep
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
+# -- Lazygit (Release binary) --
+wget https://github.com/jesseduffield/lazygit/releases/download/v0.58.1/lazygit_0.58.1_linux_x86_64.tar.gz
+tar -xzf ./laylazygit_0.58.1_linux_x86_64.tar.gz -C ./lazygit
+mv ./lazygit/lazygit $HOME/.local/bin/lazygit
+rm -rf ./lazygit*
+
