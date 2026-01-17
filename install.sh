039b0da9043726814063fe1a8fa8f1af89b04a4e
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2026-01-17 18:24:40 +0100

    Update/improve install.sh script.

--- install.sh
+++ install.sh
@@ -3,13 +3,16 @@ set -euo pipefail
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source $HOME/.cargo/env
+
 cargo install zellij
 cargo install ripgrep
+
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
 # -- Lazygit (Release binary) --
 wget https://github.com/jesseduffield/lazygit/releases/download/v0.58.1/lazygit_0.58.1_linux_x86_64.tar.gz
-tar -xzf ./laylazygit_0.58.1_linux_x86_64.tar.gz -C ./lazygit
+tar -xzf ./lazygit_0.58.1_linux_x86_64.tar.gz -C ./lazygit
 mv ./lazygit/lazygit $HOME/.local/bin/lazygit
 rm -rf ./lazygit*
-
